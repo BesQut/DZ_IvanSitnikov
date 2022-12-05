0c758fc1f55c31ed11246aee66c93bf17519a60d
Ivan <nindsia146@mail.ru>
2022-12-05 14:07:49 +0400

    DZ2

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -35,18 +35,13 @@ bool ThirdNum(int num)
 
 int Num(int num)
 {
-    if(num / 10000 != 0)
+    int n = 10;
+    if(num >= 1000)
     {
-        num = num / 100;
-    }
-    else if(num / 1000 != 0)
-    
-    {
-        num = num / 10;
-    }
-    else if(num / 100 != 0)
-    {
-        return num;
+        while(num >= 1000)
+        { 
+            num = num / n; 
+        }
     }
     return num;
 }
@@ -57,70 +52,6 @@ if(ThirdNum(num) == true)
 {
     int num1 = Num(num);
     int res = num1 % 10;
-    Console.Write($"Изначальное число: {num}, третье число: {res}");
-}
-else
-{
-    Console.Write("Нет третьего числа");
-}
-*/
-
-// Для себя. Поробывал 2 задачу с помощью массивов, получилось только до числа 1234567
-/*
-bool ThirdNum(int num)
-{
-    while(num / 100 != 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-int Num(int num)
-{
-    int num1 = num;
-    int[] array = {10, 100, 1000, 10000, 100000, 1000000};
-    if(num1 / 1000 != 0)
-    {
-        int num2;
-        int i = 0;
-        int n = 2;
-        if(num > 100000)
-        {
-            n = 4;
-        }
-        while(n > i)
-        {
-
-            if(num1 / array[n+1] != 0)
-            {
-                Console.WriteLine(n);
-                num2 = num1 / array[n-1];
-                return num2;
-            }
-            else
-            {
-                num1 = num1;
-            }
-            n--;
-            
-        }
-    }
-    else
-    {
-        num1 = num1;
-    }
-    return num1;
-}
-
-int num3;
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-if(ThirdNum(num) == true)
-{
-    num3 = Num(num);
-    int res = num3 % 10;
     Console.Write($"Изначальное число: {num}, третье число: {res}");
 }
 else
