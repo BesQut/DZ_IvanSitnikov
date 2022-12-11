de8baad99caf5395544075cf7169f7ee54a1c570
Ivan <nindsia146@mail.ru>
2022-12-11 23:41:43 +0400

    4

---
+++ DZ4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+double Quad(int a, int b)
+{
+    double quadrate = Math.Pow(a,b);
+    return quadrate;
+}
+
+Console.Write("Число, которое будут возводить в квадрат: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Степень числа: ");
+int b = Convert.ToInt32(Console.ReadLine());
+double res = Quad(a, b);
+Console.Write(res);
+*/
+
+// Задача 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int a = num % 10; // 122 --> 2
+// Console.Write(a);
+// Задача 3. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
